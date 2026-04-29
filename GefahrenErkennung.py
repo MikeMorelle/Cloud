5f6508224ea6c22b87c73dccdab04e198f80bfe2
MikeMorelle <michael.morg21@gmail.com>
2026-04-30 01:24:06 +0200

    Update YOLO model for fire and smoke

--- GefahrenErkennung.py
+++ GefahrenErkennung.py
@@ -5,71 +5,96 @@ import time
 from ultralytics import YOLO
 from skimage.metrics import structural_similarity as ssim
 
-model = YOLO("yolo11n.pt")
+#YOLO Modell laden 
+model = YOLO("best(1).pt")
 cap = cv2.VideoCapture(0)
 
 def is_occluded(frame):
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    if frame is None:
+        return False
+    
+    #Bild in Graustufen umwandeln
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
+    #Helligkeit/Varianz berechnen 
     brightness = np.mean(gray)
     variance = np.var(gray)
-
+    #Schwellenwerte
     return brightness < 50 or variance < 500
 
 def is_camera_moved(frame1, prev_frame):
     if frame1 is None or prev_frame is None:
         return False
     
+    #Bilder in Graustufen umwandeln
     gray1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
     gray2 = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
+    #Strucural Similarity Index (0-1) berechnen
     score, _ = ssim(gray1, gray2, full=True)
-
+    #Schwellenwert
     return score < 0.7
 
+#Cooldown gegen Spam-Alerts
 last_sent = 0
 COOLDOWN = 5  
 
 def send_alert(message):
+    #aktuelle und letzte Sendezeit vergleichen
     global last_sent
     now = time.time()
+    #Prüfen, ob Cooldown aktiv ist
     if now - last_sent < COOLDOWN:
-        print("Alarm Cooldwon.")
+        print("ALARM SPAM VERHINDERT")
         return
-
-    url = "***"  #erstelle selber Bot
+    #Telegram Bot API aufrufen
+    url = "https://api.telegram.org/bot8358523968:AAH43qZhtvkxW8FVYAaf-NKjxaoG2kQl9c4/sendMessage"
     data = {
-        "chat_id": "***",  #starte Bot und geh auf /getUpdates 
+        "chat_id": "8765793935",
         "text": message
     }
     requests.post(url, data=data)
-    print(f"Alarm gesendet: {message}")
+    print(f"ALARM!!! {message}")
     last_sent = now
 
+#Vorheriges Frame für Bewegungserkennung
 prev_frame = None
 
 while True:
+    #Frame von Kamera lesen
     ret, frame = cap.read()
+    #Redo wenn kein Frame gelesen werden konnte
     if not ret:
         break
-    #YOLO
+    #YOLO model auf Frame anwenden
     results = model(frame)
-    #occlusion detection
+   
+    #Verdeckenserkennung
     occluded = is_occluded(frame)
-    #camera movement detection
+    #Bewegungserkennung
     moved = is_camera_moved(frame, prev_frame)
     prev_frame = frame.copy()
-    #Display results
+
+    #Status ggf. Alarm setzen
     status = "OK"
     if occluded:
-        status = "Occluded"
-        send_alert("Kamera ist verdeckt!")
+        status = "Verdeckt"
+        send_alert("Die Kamera ist verdeckt!")
     elif moved:
-        status = "Moved"
-        send_alert("Da bewegt sich was!")
+        status = "Bewegt"
+        send_alert("ES IST BÖSES IM BUSCH!")
+    if len(results[0].boxes) > 0:
+        if results[0].boxes.cls[0] == 0:  # Klasse 0 = Feuer
+            status = "Feuer"
+        elif results[0].boxes.cls[0] == 1:  # Klasse 1 = Rauch
+            status = "Rauch"
+        send_alert("ES BRENNT!")
     
+    #Status auf Frame anzeigen
     cv2.putText(frame, status, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
     cv2.imshow("System", frame)
-    if cv2.waitKey(1) == 27: # ESC zum Aufhören
+    #ESC key to exit
+    if cv2.waitKey(1) == 27: 
         break
 
+#Ressourcen freigeben
 cap.release()
 cv2.destroyAllWindows()
